11f2bced3e3f356a31fb8d25a3efad4da009f43e
Bryan <bryan.bjc2@hotmail.com>
2024-08-26 03:25:54 -0500

    formulario basico

--- src/pages/Forms/Forms.tsx
+++ src/pages/Forms/Forms.tsx
@@ -1,37 +1,70 @@
 
-import FormCard, { FormButtons, FormImage, FormTitle } from "../../components/FormsComponents"
-import { products } from '../../data/products'
+import { ChangeEvent, useState } from 'react'
 import '../styles/custom-styles.css'
 
-const product = products[0]
-
 export const Forms = () => {
+
+    const [registerData, setRegisterData] = useState({
+        name: '',
+        email: '',
+        password1: '',
+        password2: ''
+    })
+
+    const {name, email, password1, password2} = registerData
+    
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        
+        setRegisterData( prev => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }))
+
+    }
+
+    const onSudmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log(registerData)
+    }
+
     return (
         <div>
             <h1>Forms Page</h1>
-            <hr />
-            <div>
-                <FormCard
-                    key={product.id}
-                    productForm={product}
-                    className="bg-dark text-white"
-                    initialValues={{
-                        count: 4,
-                        maxCount: 10
-                    }}
-                >
-                    {
-                        (msg) => (
-                            <>
-                                <FormImage className="custom-image" />
-                                <FormTitle className="text-bold" />
-                                <FormButtons className="custom-buttons" />
-                                <h1>{msg}</h1>
-                            </>
-                        )
-                    }
-                </FormCard>
-            </div>
+            <hr />     
+            <form
+                onSubmit={ (ev) => onSudmit(ev) }
+            >
+                <input
+                    type='text'
+                    placeholder='Name'
+                    name='name'
+                    value={name}
+                    onChange={ onChange }
+                />
+                <input
+                    type='email'
+                    placeholder='Email'
+                    name='email'
+                    value={email}
+                    onChange={ onChange }
+                />
+                <input
+                    type='password'
+                    placeholder='Password 1'
+                    name='password1'
+                    value={password1}
+                    onChange={ onChange }
+                />
+                <input
+                    type='password'
+                    placeholder='Password 2'
+                    name='password2'
+                    value={password2}
+                    onChange={ onChange }
+                />
+                <button type='submit'>Crear</button>
+            </form>
         </div>
     )
 }
